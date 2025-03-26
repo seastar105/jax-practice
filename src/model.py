fix bug on attention masking, add training loop

src/model.py
@@ -57,13 +57,12 @@ class FeedForward(nnx.Module):
 
 def scaled_dot_product_attention(q, k, v, mask: Optional[jnp.array] = None, dropout: Optional[nnx.Dropout] = None):
     # q, k, v: (..., seq_len, dim)
-    # mask logits where mask is False
     scale = 1 / jnp.sqrt(q.shape[-1])
 
     logits = einsum(q, k, "b ... q d, b ... k d -> b ... q k") * scale
     if mask is not None:
         mask_value = -jnp.finfo(logits.dtype).max
-        logits = jnp.where(mask, logits, mask_value)
+        logits = jnp.where(mask, mask_value, logits)
 
     attn = nnx.softmax(logits, axis=-1)
     if dropout is not None:

src/torch_version/model.py
@@ -4,7 +4,8 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
-from cs336_basics.bbpe import Tokenizer
+
+from src.bbpe import Tokenizer
 
 
 def gelu(x: torch.Tensor):

src/torch_version/train.py
@@ -8,9 +8,10 @@ from typing import Optional, Union
 import numpy as np
 import torch
 import wandb
-from cs336_basics.model import TransformerLM, cross_entropy_loss
-from cs336_basics.optimizer import AdamW, clip_grad_norm, get_cosine_schedule_lr
 from tqdm.auto import tqdm
+
+from src.torch_version.model import TransformerLM, cross_entropy_loss
+from src.torch_version.optimizer import AdamW, clip_grad_norm, get_cosine_schedule_lr
 
 torch.set_float32_matmul_precision("high")
 torch.backends.cuda.matmul.allow_tf32 = True